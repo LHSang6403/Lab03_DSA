ab3841e2d53860c67af1139add23af1374b7fd20
Kha7734 <huynhsikha2003@gmail.com>
2022-06-07 22:50:52 +0700

    Add ShellSort

--- Sort.cpp
+++ Sort.cpp
@@ -15,8 +15,8 @@ void ShakerSort(int arr[], int n, int &comparision, double &time)
 {
     int up = 0,
         down = n - 1,
-        hv = 0; //store the last position of each swap.
-    
+        hv = 0; // store the last position of each swap.
+
     clock_t start, end;
     comparision = 0;
     time = 0;
@@ -46,11 +46,55 @@ void ShakerSort(int arr[], int n, int &comparision, double &time)
         up = hv;
     }
     end = clock();
-    time = (double)(end - start)/CLOCKS_PER_SEC;
+    time = (double)(end - start) / CLOCKS_PER_SEC;
 }
 
-void ShellSort(int a[], int n, int gap[], int gap_num){
-    
+int createGap(int n, int gap[], int &gap_num, int &comparision)
+{
+    gap[0] = 1;
+    for (int i = 1; gap[i - 1] < n; i++)
+    {
+        ++comparision;
+        gap[i] = 3 * gap[i - 1] + 1;
+        gap_num++;
+    }
+}
+
+void ShellSort(int a[], int n, int &comparision, double &time)
+{   
+    clock_t start, end;
+    comparision = 0, time = 0;
+
+    int *gap = new int[n],
+         gap_num = 0;
+    start = clock();
+    createGap(n, gap, gap_num, comparision);
+
+    int p = 0,
+        j = 0,
+        t = 0,
+        x = 0;
+    for (p = 0; p < gap_num; p++)
+    {
+        ++comparision;
+        t = gap[p];
+        for (int i = t; i < n; i++)
+        {
+            ++comparision;
+            x = a[i];
+            j = i;
+            while (++comparision && ++comparision && x < a[j - t] && j >= t)
+            {
+                a[j] = a[j - t];
+                j = j - t;
+            }
+            a[j] = x;
+        }
+    }
+    delete[] gap;
+
+    end = clock();
+    time = (double) (end - start)/CLOCKS_PER_SEC;
 }
 
 void QUICKSORT(int a[], int l, int r, int &comparision)
@@ -93,14 +137,16 @@ void CountingSort(int *&a, int n, int &comparision, double &time)
     clock_t start, end;
     comparision = 0;
 
-    // According to randomize code, the elements' range is from 1 to n-1 
-    int *flag = new int [n]{0};
-    int *res = new int [n];
+    // According to randomize code, the elements' range is from 1 to n-1
+    int *flag = new int[n]{0};
+    int *res = new int[n];
 
     start = clock();
-    for (int i = 0; ++comparision, i < n; i++) flag[a[i]]++;
-    for (int i = 1; ++comparision, i < n; i++) flag[i] += flag[i-1];
-    for (int i = 0; ++comparision, i < n; i++) 
+    for (int i = 0; ++comparision, i < n; i++)
+        flag[a[i]]++;
+    for (int i = 1; ++comparision, i < n; i++)
+        flag[i] += flag[i - 1];
+    for (int i = 0; ++comparision, i < n; i++)
     {
         res[flag[a[i]] - 1] = a[i];
         flag[a[i]]--;
@@ -112,7 +158,7 @@ void CountingSort(int *&a, int n, int &comparision, double &time)
     delete flag;
 
     end = clock();
-    time = (double) (end - start) / CLOCKS_PER_SEC;
+    time = (double)(end - start) / CLOCKS_PER_SEC;
     // Ref: https://www.geeksforgeeks.org/counting-sort/
 }
 
@@ -122,14 +168,16 @@ void RadixSort(int *&a, int n, int &comparision, double &time)
     comparision = 0;
 
     start = clock();
-    for (int i = 1; ++comparision, i <= pow(10,(int)log10(n) + 1); i *= 10)
+    for (int i = 1; ++comparision, i <= pow(10, (int)log10(n) + 1); i *= 10)
     {
-        int *res = new int [n]{0};
-        int flag[10]={0};
+        int *res = new int[n]{0};
+        int flag[10] = {0};
 
-        for (int j = 0; ++comparision, j < n; j++) flag[(a[j] / i) % 10]++;
-        for (int j = 1; ++comparision, j < 10; j++) flag[j] += flag[j-1];
-        for (int j = n - 1; ++comparision, j >= 0; j--) 
+        for (int j = 0; ++comparision, j < n; j++)
+            flag[(a[j] / i) % 10]++;
+        for (int j = 1; ++comparision, j < 10; j++)
+            flag[j] += flag[j - 1];
+        for (int j = n - 1; ++comparision, j >= 0; j--)
         {
             res[flag[(a[j] / i) % 10] - 1] = a[j];
             flag[(a[j] / i) % 10]--;
@@ -141,6 +189,6 @@ void RadixSort(int *&a, int n, int &comparision, double &time)
     }
     end = clock();
 
-    time = (double) (end - start) / CLOCKS_PER_SEC;
+    time = (double)(end - start) / CLOCKS_PER_SEC;
     // Ref: https://www.geeksforgeeks.org/radix-sort/
 }
--- Sort.h
+++ Sort.h
@@ -1,4 +1,5 @@
 void ShakerSort(int arr[], int n, int &comparision, double &time);
+void ShellSort(int a[], int n, int &comparision, double &time);
 void QuickSort(int a[], int n, int &comparision, double &time);
 void CountingSort(int *&a, int n, int &comparision, double &time);
 void RadixSort(int *&a, int n, int &comparision, double &time);
