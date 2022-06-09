52ce1c3abbfe204b5ca4681ee6d56736059cdcb3
Xang <88763282+LHSang6403@users.noreply.github.com>
2022-06-09 20:54:36 +0700

    Update Sort.h

--- Sort.h
+++ Sort.h
@@ -4,3 +4,6 @@ void QuickSort(int a[], int n, int &comparison, double &time);
 void CountingSort(int *&a, int n, int &comparison, double &time);
 void RadixSort(int *&a, int n, int &comparison, double &time);
 void FlashSort(int arr[], int n, int &comparison, double &time);
+void buildHeap(int arr[], int n, int i, int &comparison, double &time);
+void HeapSort(int arr[], int sz, int &comparison, double &time);
+void BubbleSort(int arr[], int sz, int &comparison, double &time);
